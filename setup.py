93108d8a50d302ce50cb391a4215c92fd8857282
Ubuntu <ubuntu@ip-172-31-40-44.eu-west-1.compute.internal>
2016-12-21 10:22:06 +0000

    Update the setup.py to install our fork

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ from setuptools import find_packages
 setup(name='Keras',
       version='1.2.0',
       description='Deep Learning for Python',
-      author='Francois Chollet',
-      author_email='francois.chollet@gmail.com',
-      url='https://github.com/fchollet/keras',
-      download_url='https://github.com/fchollet/keras/tarball/1.2.0',
+      author='Clement Thorey',
+      author_email='clement.thorey@gmail.com',
+      url='https://github.com/xihelm/keras',
       license='MIT',
       install_requires=['theano', 'pyyaml', 'six'],
       extras_require={
