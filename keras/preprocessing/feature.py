57659059ed2a6ce195cd7f805256af4ed2ce9704
cthorey <clement.thorey@gmail.com>
2017-01-16 16:28:12 +0000

    Clean up H5FeatureIterator

--- keras/preprocessing/feature.py
+++ keras/preprocessing/feature.py
@@ -17,31 +17,37 @@ class H5FeatureIterator(Iterator):
 
     def __init__(self, feature_path,
                  split,
-                 batch_size=5,
+                 batch_size=None,
                  seed=None):
         self.feature_path = feature_path
         self.split = split
-        self.batch_size = batch_size
         self.hf = ojoin(feature_path, 'feature.h5')
         config = json.load(
             open(ojoin(feature_path, 'feature_config.json'), 'r'))
         self.__dict__.update(config)
+        if batch_size is not None:
+            self.batch_size = batch_size
         self.nb_sample = self.get_nsample(split)
+        if self.batch_size > self.nb_sample:
+            self.batch_size = self.nb_sample
+            print('Set batch_size to {}'.format(self.nb_sample))
         # DONT CHANGE shuffle - reason in the docstring
         super(H5FeatureIterator, self).__init__(
-            self.nb_sample, batch_size=batch_size, shuffle=False, seed=seed)
+            self.nb_sample, batch_size=self.batch_size, shuffle=False, seed=seed)
 
     def get_nsample(self, split):
-        if getattr(self, 'nsample_{}'.format(split)) is None:
-            nsample = getattr(self, '{}_nb_sample'.format(split))
-        else:
-            nsample = getattr(self, 'nsample_{}'.format(split))
+        nsample = getattr(self, '{}_nb_sample'.format(split))
         return nsample
 
     def get(self, key, chunk):
         chunk = list(chunk)  # make sure this is a list
         with h5py.File(self.hf) as hf:
-            arr = hf.get(key)[chunk]
+            if isinstance(hf[key], h5py.Dataset):
+                arr = hf.get(key)[chunk]
+            else:
+                arr = dict.fromkeys(hf[key].keys())
+                for key, value in hf[key].items():
+                    arr[key] = value[chunk]
         return arr
 
     def next(self):
